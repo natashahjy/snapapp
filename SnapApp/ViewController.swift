3e28050db2a74fae03afcd7bc0268e6dd2471d03
ITP312 <itp312@l632-43.local>
2017-05-30 10:39:12 +0800

    Initial Commit

---
+++ SnapApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapApp
+//
+//  Created by ITP312 on 30/5/17.
+//  Copyright © 2017 ITP312. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
